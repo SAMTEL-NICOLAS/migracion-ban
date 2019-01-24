fb6370820629f92cc85a2a328a85c298be0311a9
Jeferson <jeferson.david96@gmail.com>
2019-01-24 15:40:04 -0500

    24-01-2019

---
+++ src/co/com/samtel/entity/manual/csv/BigDesendeudeseIdCsv.java
@@ -0,0 +1,11 @@
+package co.com.samtel.entity.manual.csv;
+
+import java.io.Serializable;
+
+public class BigDesendeudeseIdCsv implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
