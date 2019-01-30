775fc3d3287b843c62f7a66f14058a5dbaf33d90
Carolina <diana.cardozo@samtel.co>
2019-01-30 10:40:43 -0500

    30/01/2019

--- src/co/com/samtel/cargue/enumeraciones/tables/TypeBigInfoFinancieraColumn.java
+++ src/co/com/samtel/cargue/enumeraciones/tables/TypeBigInfoFinancieraColumn.java
@@ -6,15 +6,15 @@ import java.util.Date;
 @SuppressWarnings("all")
 public enum TypeBigInfoFinancieraColumn implements IColumn {
 	
-	I_COD_OFICINA(0, "i_cod_oficina", String.class), 
-	M_GASTO_ADMINISTRATIVO(1, "m_gasto_administrativo", BigDecimal.class),
-	M_GASTO_PERSONAL(2, "m_gasto_personal", BigDecimal.class),
-	M_GASTO_NETO_PROVISION(3, "m_gasto_neto_provision", BigDecimal.class),
-	M_GASTO_ASIGNADO(4, "m_gasto_asignado", BigDecimal.class),
-	M_COSTO_PATRIMONIO_RIESGO(5, "m_costo_patrimonio_riesgo", BigDecimal.class),
-	D_FECHA_CORTE(6, "d_fecha_corte", Date.class),
-	M_INGRESOS_TOTALES(7, "m_ingresos_totales", BigDecimal.class),
-	M_EGRESOS_TOTALES(8, "m_egresos_totales", BigDecimal.class);
+	I_COD_OFICINA(0, "I_cod_oficina", String.class), 
+	M_GASTO_ADMINISTRATIVO(1, "M_gasto_administrativo", BigDecimal.class),
+	M_GASTO_PERSONAL(2, "M_gasto_personal", BigDecimal.class),
+	M_GASTO_NETO_PROVISION(3, "M_gasto_neto_provision", BigDecimal.class),
+	M_GASTO_ASIGNADO(4, "M_gasto_asignado", BigDecimal.class),
+	M_COSTO_PATRIMONIO_RIESGO(5, "M_costo_patrimonio_riesgo", BigDecimal.class),
+	D_FECHA_CORTE(6, "D_fecha_corte", Date.class),
+	M_INGRESOS_TOTALES(7, "M_ingresos_totales", BigDecimal.class),
+	M_EGRESOS_TOTALES(8, "M_egresos_totales", BigDecimal.class);
 
 
 	private int indice;
