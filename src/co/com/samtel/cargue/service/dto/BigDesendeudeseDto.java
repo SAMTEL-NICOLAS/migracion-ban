b052056db318701e7eabd365487aba9f9819f079
Carolina <diana.cardozo@samtel.co>
2019-01-30 15:06:25 -0500

    30/01/2019

--- src/co/com/samtel/cargue/service/dto/BigDesendeudeseDto.java
+++ src/co/com/samtel/cargue/service/dto/BigDesendeudeseDto.java
@@ -2,38 +2,35 @@ package co.com.samtel.cargue.service.dto;
 
 public class BigDesendeudeseDto {
 
-	private String tipoDocumento;
-	private String identificacionCliente;
-	private String codigoCliente;
-
-	public String getTipoDocumento() {
-		return tipoDocumento;
+	private String I_tipo_documento;
+	private String S_identificacion_cliente;
+	private String I_codigo_cliente;
+	
+	
+	public String getI_tipo_documento() {
+		return I_tipo_documento;
 	}
-
-	public void setTipoDocumento(String tipoDocumento) {
-		this.tipoDocumento = tipoDocumento;
+	public void setI_tipo_documento(String i_tipo_documento) {
+		I_tipo_documento = i_tipo_documento;
 	}
-
-	public String getIdentificacionCliente() {
-		return identificacionCliente;
+	public String getS_identificacion_cliente() {
+		return S_identificacion_cliente;
 	}
-
-	public void setIdentificacionCliente(String identificacionCliente) {
-		this.identificacionCliente = identificacionCliente;
+	public void setS_identificacion_cliente(String s_identificacion_cliente) {
+		S_identificacion_cliente = s_identificacion_cliente;
 	}
-
-	public String getCodigoCliente() {
-		return codigoCliente;
+	public String getI_codigo_cliente() {
+		return I_codigo_cliente;
 	}
-
-	public void setCodigoCliente(String codigoCliente) {
-		this.codigoCliente = codigoCliente;
+	public void setI_codigo_cliente(String i_codigo_cliente) {
+		I_codigo_cliente = i_codigo_cliente;
 	}
-
 	@Override
 	public String toString() {
-		return "BigDesendeudeseDto [tipoDocumento=" + tipoDocumento + ", identificacionCliente=" + identificacionCliente
-				+ ", codigoCliente=" + codigoCliente + "]";
+		return "BigDesendeudeseDto [I_tipo_documento=" + I_tipo_documento + ", S_identificacion_cliente="
+				+ S_identificacion_cliente + ", I_codigo_cliente=" + I_codigo_cliente + "]";
 	}
+
+
 
 }
