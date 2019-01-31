e21c9755b0776c550e2082cf6b0d897b952111a7
Jeferson <jeferson.david96@gmail.com>
2019-01-31 15:22:21 -0500

    31-01-2019

--- src/co/com/samtel/restservice/MigracionExcelRest.java
+++ src/co/com/samtel/restservice/MigracionExcelRest.java
@@ -11,22 +11,21 @@ import javax.ws.rs.core.MediaType;
 
 import co.com.samtel.cargue.enumeraciones.TypeFile;
 import co.com.samtel.cargue.service.IExecutePersistTable;
-import co.com.samtel.controller.IManagedBean;
 import co.com.samtel.migration.IExecuteMigration;
 
 @Stateless
 @Path("/v.1/migracionExcel")
-public class MigracionExcelRest implements IManagedBean {
-	
-	@EJB(beanName = "executePersistTable")
-	IExecutePersistTable executePersistTable;
-	
-	@EJB(beanName = "executeMigration")
-	IExecuteMigration executeMigration;
-	
+public class MigracionExcelRest {
+
 	private HashMap<String, String> listDelimiters;
 
 	private String delimiter;
+
+	@EJB(beanName = "executeMigration")
+	IExecuteMigration executeMigration;
+
+	@EJB(beanName = "executePersistTable")
+	IExecutePersistTable executePersistTable;
 
 	@GET
 	@Path("/realizarProceso")
@@ -36,17 +35,17 @@ public class MigracionExcelRest implements IManagedBean {
 		try {
 			System.out.println("001");
 
-			String url = "C:\\BigDesendeudese.csv";
+			String url = "C:\\ArchivosExcel\\BIG_DESENDEUDESE.csv";
 			setDelimiter(",");
 
 			System.out.println("002");
 			Boolean respuesta = executePersistTable.executeProcess(url, TypeFile.BIG_DESENDEUDESE, getDelimiter());
 			System.out.println("005");
 			if (respuesta) {
-				addMessage(TYPEMESSAGE.SUCCESS, " MAPEO REALIZADO CORRECTAMENTE");
+				System.out.println("MAPEO REALIZADO CORRECTAMENTE");
 				proceso = "MAPEO REALIZADO CORRECTAMENTE";
 			} else {
-				addMessage(TYPEMESSAGE.ERROR, " Error al mapear el excel");
+				System.out.println("Error al mapear el excel");
 				proceso = "Error al mapear el excel";
 			}
 
