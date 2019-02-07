79550c9a07de4b04693346504f02021996ea5cba
Jeferson <jeferson.david96@gmail.com>
2019-02-07 15:46:01 -0500

    07-02-2019

--- src/co/com/samtel/restservice/AuditMigrationRest.java
+++ src/co/com/samtel/restservice/AuditMigrationRest.java
@@ -2,7 +2,7 @@ package co.com.samtel.restservice;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.websocket.server.PathParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
